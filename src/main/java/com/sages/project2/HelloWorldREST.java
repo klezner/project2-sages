e4e1ee2c495aab4c95e6e3f5ce4f84229cc80c46
Maciej Oklinski <mackoo.oklinski@gmail.com>
2022-05-18 13:27:31 +0100

    CreateBasicEndpoint

---
+++ src/main/java/com/sages/project2/HelloWorldREST.java
@@ -0,0 +1,18 @@
+package com.sages.project2;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldREST {
+
+    @RequestMapping("/hello")
+    public ResponseEntity<String> see(){
+        return ResponseEntity.ok()
+                .body(get());
+    }
+    public String get(){
+        return "witajSwiecie";
+    }
+}
