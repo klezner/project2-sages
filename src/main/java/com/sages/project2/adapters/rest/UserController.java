90205a573072a1fb231d3579eda9ae3f5822d833
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-05-26 09:47:45 +0200

    Update UserController.java

--- src/main/java/com/sages/project2/adapters/rest/UserController.java
+++ src/main/java/com/sages/project2/adapters/rest/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
