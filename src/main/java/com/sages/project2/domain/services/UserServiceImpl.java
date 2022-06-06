0b150511fc66653d86b0b0dea72a31fe614e7379
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-06-06 15:28:06 +0200

    before meeting

--- src/main/java/com/sages/project2/domain/services/UserServiceImpl.java
+++ src/main/java/com/sages/project2/domain/services/UserServiceImpl.java
@@ -5,9 +5,11 @@ import com.sages.project2.domain.ports.in.UserService;
 import com.sages.project2.domain.ports.out.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
