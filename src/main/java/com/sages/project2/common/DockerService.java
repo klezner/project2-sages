b51292a9ddfb1d2297127566e95f8827f2d7c88f
Geri999 <gerard@bci.pl>
2022-06-14 21:22:00 +0200

    DockerService stage 1

--- src/main/java/com/sages/project2/common/DockerService.java
+++ src/main/java/com/sages/project2/common/DockerService.java
@@ -11,71 +11,75 @@ import com.spotify.docker.client.messages.ContainerInfo;
 import com.spotify.docker.client.messages.ExecCreation;
 import lombok.SneakyThrows;
 import org.apache.catalina.startup.HostConfig;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
+@Service
 public class DockerService {
+
     @SneakyThrows
-    public static String dockerService(String gitURL) /*throws DockerCertificateException, DockerException, InterruptedException*/ {
-        DockerClient docker = DefaultDockerClient.fromEnv().build();
-        docker.pull("geri2045/ad:aadd");
+    public String dockerService(String gitURL) {
+        DockerClient docker = null;
+        String id = null;
+        List<String> output = null;
 
-        //Prepare volume
-//        HostConfig.Bind volume = com.spotify.docker.client.messages.HostConfig.Bind.from("/j/").to("/home/gerard3")/*.readOnly(false)*/.build();
+        try {
+            docker = DefaultDockerClient.fromEnv().build();
+            docker.pull("geri2045/ad:aadd");
 
-        //Prepare host
-//        final HostConfig hostConfig = HostConfig.builder().appendBinds(volume).build();
+            //Prepare container
+            ContainerConfig containerConfig = ContainerConfig.builder()
+                    //                .hostConfig(hostConfig)
+                    .image("geri2045/ad:aadd")
+                    .cmd("sh", "-c", "while :; do sleep 1; done")
+                    .build();
+            ContainerCreation creation = docker.createContainer(containerConfig, "GerardContainer_______");
+            id = creation.id();
+            ContainerInfo info = docker.inspectContainer(id);
 
-        //Prepare container
-        ContainerConfig containerConfig = ContainerConfig.builder()
-//                .hostConfig(hostConfig)
-                .image("geri2045/ad:aadd")
-                .cmd("sh", "-c", "while :; do sleep 1; done")
-                .build();
-        ContainerCreation creation = docker.createContainer(containerConfig, "GerardContainer_______");
-        String id = creation.id();
-        ContainerInfo info = docker.inspectContainer(id);
+            // Start container
+            docker.startContainer(id);
+            System.out.println(id);
 
-        // Start container
-        docker.startContainer(id);
-        System.out.println(id);
-
-        String tempFolderName = gitURL.split("//")[gitURL.split("//").length - 1].replace(".git", "");
+            String tempFolderName = gitURL.split("/")[gitURL.split("/").length - 1].replace(".git", "").toLowerCase();
 
 
-        // Start block of commands
-        ArrayList<String[]> listOfCommands = new ArrayList<>();
-        listOfCommands.add(new String[]{"java", "-version"});
+            // Start block of commands
+            ArrayList<String[]> listOfCommands = new ArrayList<>();
+            listOfCommands.add(new String[]{"java", "-version"});
 //        listOfCommands.add(new String[]{"git", "clone", "https://github.com/Coveros/helloworld.git"});
-        listOfCommands.add(new String[]{"git", "clone", gitURL});
-        listOfCommands.add(new String[]{"sh", "-c", "ls -la"});
-        listOfCommands.add(new String[]{"sh", "-c", String.format("cd /%s/ && ls -la", tempFolderName)});
-        listOfCommands.add(new String[]{"sh", "-c", String.format("cd /%s/ && mvn clean package", tempFolderName)});
-        listOfCommands.add(new String[]{"sh", "-c", String.format("cd /%s/target/ && java -jar %s-1-1.jar", tempFolderName, tempFolderName.toLowerCase()    )});
+            listOfCommands.add(new String[]{"git", "clone", gitURL});
+            listOfCommands.add(new String[]{"sh", "-c", "ls -la"});
+            listOfCommands.add(new String[]{"sh", "-c", String.format("cd /%s/ && ls -la", tempFolderName)});
+            listOfCommands.add(new String[]{"sh", "-c", String.format("cd /%s/ && mvn clean package", tempFolderName)});
+            listOfCommands.add(new String[]{"sh", "-c", String.format("cd /%s/target/ && ls -la", tempFolderName)});
+            listOfCommands.add(new String[]{"sh", "-c", String.format("mv /%s/target/*.jar /%s/target/go.jar", tempFolderName, tempFolderName)});
+            listOfCommands.add(new String[]{"sh", "-c", String.format("cd /%s/target/ && java -jar go.jar", tempFolderName, tempFolderName)});
 //        listOfCommands.add(new String[]{"sh", "-c", "cd /helloworld/target/ && java -cp helloworld-1.1.jar com.coveros.demo.helloworld.HelloWorld"});
 
-        // todo: -jar
+            // Execute commands
+            output = new LinkedList<>();
 
-        // Execute commands
-        List<String> output = new LinkedList<>();
+            String finalId = id;
+            DockerClient finalDocker = docker;
+            listOfCommands.stream().map(s -> sendToContainer(s, finalId, finalDocker)).forEach(output::add);
+            output.forEach(System.out::println);
 
-        listOfCommands.stream().map(s -> sendToContainer(s, id, docker)).forEach(output::add);
-        output.forEach(System.out::println);
+            // Stop container
+            System.out.println("closing...");
+        } catch (DockerCertificateException | DockerException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            docker.killContainer(id);
+            docker.removeContainer(id);
+            docker.close();
 
-        // Wait!
-        Scanner scanner = new Scanner(System.in);
-        scanner.nextLine();
-
-        // Stop container
-        System.out.println("closing...");
-        docker.killContainer(id);
-        docker.removeContainer(id);
-        docker.close();
-
-        return output.get(output.size()-1);
+        }
+            return output.get(output.size() - 1);
     }
 
     public static String sendToContainer(String[] commands, String id, DockerClient docker) {
@@ -91,4 +95,3 @@ public class DockerService {
         return output.readFully();
     }
 }
-
