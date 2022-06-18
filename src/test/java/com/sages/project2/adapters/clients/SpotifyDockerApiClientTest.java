c6ed9b97df53f7786f2bb21078f16edff8f581a1
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-06-18 22:38:11 +0200

    minor change

---
+++ src/test/java/com/sages/project2/adapters/clients/SpotifyDockerApiClientTest.java
@@ -0,0 +1,11 @@
+package com.sages.project2.adapters.clients;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class SpotifyDockerApiClientTest {
+
+    @Test
+    void checkSolution() {
+    }
+}
