65022a10c49a6c5267e685702b49b0df289d1158
bartmj <bartek.m.janusz@gmail.com>
2022-06-20 23:25:11 +0200

    Update SpotifyDockerApiClient.java
    Wyjątki

--- src/main/java/com/sages/project2/adapters/clients/SpotifyDockerApiClient.java
+++ src/main/java/com/sages/project2/adapters/clients/SpotifyDockerApiClient.java
@@ -8,6 +8,7 @@ import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import com.sages.project2.domain.ports.out.DockerApiClient;
+import org.springframework.stereotype.Service;
 
 import java.util.*;
 
@@ -20,55 +21,66 @@ public class SpotifyDockerApiClient implements DockerApiClient {
     public static String GH_TOKEN = "GH_TOKEN_GH_TOKEN";
 
     public String checkSolution(String repoName, String branchName) throws DockerCertificateException, DockerException, InterruptedException {
-        final DockerClient docker = DefaultDockerClient.fromEnv().build();
+        DockerClient docker = null;
+        String id = null;
+        List<String> output;
 
-        docker.pull(DOCKER_IMAGE);
+        try {
+            docker = DefaultDockerClient.fromEnv().build();
+            docker.pull("geri2045/ad:aadd");
 
-        final String[] ports = {"10002"};
-        final Map<String, List<PortBinding>> portBindings = new HashMap<>();
-        for (String port : ports) {
-            List<PortBinding> hostPorts = new ArrayList<>();
-            hostPorts.add(PortBinding.of("0.0.0.0", port));
-            portBindings.put(port, hostPorts);
+            final String[] ports = {"10002"};
+            final Map<String, List<PortBinding>> portBindings = new HashMap<>();
+            for (String port : ports) {
+                List<PortBinding> hostPorts = new ArrayList<>();
+                hostPorts.add(PortBinding.of("0.0.0.0", port));
+                portBindings.put(port, hostPorts);
+            }
+
+            List<PortBinding> randomPort = new ArrayList<>();
+            randomPort.add(PortBinding.randomPort("0.0.0.0"));
+            portBindings.put("443", randomPort);
+
+            final HostConfig hostConfig = HostConfig.builder()
+                    .binds(PATH_TO_LOCAL_M2_FOLDER + ":/root/.m2/")
+                    .portBindings(portBindings).build();
+
+
+            final ContainerConfig containerConfig = ContainerConfig.builder()
+                    .hostConfig(hostConfig)
+                    .image(DOCKER_IMAGE).exposedPorts(ports)
+                    .cmd("sh", "-c", "while :; do sleep 1; done;")
+                    .build();
+
+            final ContainerCreation creation = docker.createContainer(containerConfig);
+            id = creation.id();
+
+            final ContainerInfo info = docker.inspectContainer(id);
+
+            docker.startContainer(id);
+
+            ArrayList<String[]> listOfCommands = new ArrayList<>();
+
+            listOfCommands.add(new String[]{"git", "clone", "--branch", branchName,
+                    "--single-branch", "https://" + GH_TOKEN + "@github.com/" + GITHUB_ADMIN_USERNAME + "/" + repoName + ".git"});
+
+            listOfCommands.add(new String[]{"sh", "-c", "cd " + repoName + " && mvn test"});
+
+            output = new LinkedList<>();
+            String finalId = id;
+            DockerClient finalDocker = docker;
+
+            listOfCommands.stream().map(s -> sendToContainer(s, finalId, finalDocker)).forEach(output::add);
+
+            System.out.println("closing...");
+        } catch (DockerCertificateException | DockerException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            docker.killContainer(id);
+            docker.removeContainer(id);
+            docker.close();
+
         }
-
-        List<PortBinding> randomPort = new ArrayList<>();
-        randomPort.add(PortBinding.randomPort("0.0.0.0"));
-        portBindings.put("443", randomPort);
-
-        final HostConfig hostConfig = HostConfig.builder()
-                .binds(PATH_TO_LOCAL_M2_FOLDER + ":/root/.m2/")
-                .portBindings(portBindings).build();
-
-
-        final ContainerConfig containerConfig = ContainerConfig.builder()
-                .hostConfig(hostConfig)
-                .image(DOCKER_IMAGE).exposedPorts(ports)
-                .cmd("sh", "-c", "while :; do sleep 1; done;")
-                .build();
-
-        final ContainerCreation creation = docker.createContainer(containerConfig);
-        final String id = creation.id();
-
-        final ContainerInfo info = docker.inspectContainer(id);
-
-        docker.startContainer(id);
-
-        ArrayList<String[]> listOfCommands = new ArrayList<>();
-
-        listOfCommands.add(new String[]{"git", "clone", "--branch", branchName,
-                "--single-branch", "https://" + GH_TOKEN + "@github.com/" + GITHUB_ADMIN_USERNAME + "/" + repoName + ".git"});
-
-        listOfCommands.add(new String[]{"sh", "-c", "cd " + repoName + " && mvn test"});
-        List<String> output = new LinkedList<>();
-
-        listOfCommands.stream().map(s -> sendToContainer(s, id, docker)).forEach(output::add);
-
-        System.out.println("closing...");
-        docker.killContainer(id);
-        docker.removeContainer(id);
-        docker.close();
-
         return output.get(output.size() - 1);
     }
 
