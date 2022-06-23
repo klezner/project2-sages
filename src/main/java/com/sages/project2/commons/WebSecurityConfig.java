be7f425f1231fe974cfa7a13cc7a77c574eb7831
Bartłomiej Janusz <bartek.m.janusz@gmail.com>
2022-06-23 13:08:03 +0200

    security uncommented

--- src/main/java/com/sages/project2/commons/WebSecurityConfig.java
+++ src/main/java/com/sages/project2/commons/WebSecurityConfig.java
@@ -1,76 +1,76 @@
-//package com.sages.project2.commons;
-//
-//
-//import com.sages.project2.adapters.persistence.entities.QuestEntity;
-//import com.sages.project2.adapters.persistence.entities.UserEntity;
-//import com.sages.project2.adapters.persistence.repositories.JpaUserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.AuthorityUtils;
-//import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-//import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import java.util.Collection;
-//import java.util.Collections;
-//import java.util.HashSet;
-//import java.util.Map;
-//import java.util.Optional;
-//import java.util.Set;
-//
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-//@RequiredArgsConstructor
-//public class WebSecurityConfig {
-//
-//    private static final String LOGIN_ATTRIBUTE = "login";
-//    private static final String ROLE_USER = "ROLE_USER";
-//    private final JpaUserRepository jpaUserRepository;
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/").permitAll()
-//                .antMatchers("/h2-console/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .oauth2Login(oauth2 -> oauth2
-//                        .userInfoEndpoint(userInfo -> userInfo
-//                                .userAuthoritiesMapper(this.userAuthoritiesMapper())))
-//                .logout()
-//                .invalidateHttpSession(true)
-//                .deleteCookies("JSESSIONID");
-//        http.csrf().disable();
-//        return http.build();
-//    }
-//
-//    private GrantedAuthoritiesMapper userAuthoritiesMapper() {
-//        return (authorities) -> {
-//            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
-//
-//            authorities.forEach(authority -> {
-//                if (authority instanceof OAuth2UserAuthority oauth2UserAuthority) {
-//
-//                    Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
-//
-//                    String login = (String) userAttributes.get(LOGIN_ATTRIBUTE);
-//
-//                    Set<QuestEntity> quests = Collections.emptySet();
-//                    Optional.ofNullable(jpaUserRepository.findByLogin(login))
-//                            .ifPresentOrElse(
-//                                    user -> mappedAuthorities
-//                                            .addAll(AuthorityUtils.commaSeparatedStringToAuthorityList(user.getRole())),
-//                                    () -> jpaUserRepository.save(new UserEntity(login, ROLE_USER, quests)));
-//                }
-//            });
-//
-//            return mappedAuthorities;
-//        };
-//    }
-//}
+package com.sages.project2.commons;
+
+
+import com.sages.project2.adapters.persistence.entities.QuestEntity;
+import com.sages.project2.adapters.persistence.entities.UserEntity;
+import com.sages.project2.adapters.persistence.repositories.JpaUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private static final String LOGIN_ATTRIBUTE = "login";
+    private static final String ROLE_USER = "ROLE_USER";
+    private final JpaUserRepository jpaUserRepository;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userAuthoritiesMapper(this.userAuthoritiesMapper())))
+                .logout()
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID");
+        http.csrf().disable();
+        return http.build();
+    }
+
+    private GrantedAuthoritiesMapper userAuthoritiesMapper() {
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+
+            authorities.forEach(authority -> {
+                if (authority instanceof OAuth2UserAuthority oauth2UserAuthority) {
+
+                    Map<String, Object> userAttributes = oauth2UserAuthority.getAttributes();
+
+                    String login = (String) userAttributes.get(LOGIN_ATTRIBUTE);
+
+                    Set<QuestEntity> quests = Collections.emptySet();
+                    Optional.ofNullable(jpaUserRepository.findByLogin(login))
+                            .ifPresentOrElse(
+                                    user -> mappedAuthorities
+                                            .addAll(AuthorityUtils.commaSeparatedStringToAuthorityList(user.getRole())),
+                                    () -> jpaUserRepository.save(new UserEntity(login, ROLE_USER, quests)));
+                }
+            });
+
+            return mappedAuthorities;
+        };
+    }
+}
